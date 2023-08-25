ce0e9ae2d17beebd396a482d1399628b9510b37a
Diego8541265 <142626548+Diego8541265@users.noreply.github.com>
2023-08-25 13:54:59 -0500

    nil

--- loginuser.php
+++ loginuser.php
@@ -1,5 +1,13 @@
 <?php
 
 require_once("conexion.php");
-if(!empty($_POST['nombres']) && !empty($_POST['apellidos']) && !empty($_POST['correo']) && !empty($_POST['tipDoc']) && !empty($_POST['numDoc']) && !empty($_POST['Contraseña']))
+if(!empty($_POST['nombres']) && !empty($_POST['apellidos']) && !empty($_POST['correo']) && !empty($_POST['tipDoc']) && !empty($_POST['numDoc']) && !empty($_POST['contraseña']) ) {
+
+    $name = $_POST['nombres'];
+    $name = $_POST['apellidos'];
+    $name = $_POST['correo'];
+    $name = $_POST['tipDoc'];
+    $name = $_POST['numDoc'];
+    $name = $_POST[''];
+}
 ?>
