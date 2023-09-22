53d5e6f99ed42fe2e74c33c218533913361d67be
miyamiura <140010323+Shinrag@users.noreply.github.com>
2023-09-22 16:42:18 -0500

    a

--- subir_Guias.php
+++ subir_Guias.php
@@ -21,40 +21,7 @@
    
     </nav>
 </div>
-<header>
-    <div>
-        <h1 style="color: rgb(4, 0, 255);">Hola</h1>
-    </div>
-    <div>
-    <?php session_start(); ?>
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="UTF-8"/>
-<title>Login.php</title>
-</head>
-<body>
-<?php
-// Esto devolverá todos los archivos de esa carpeta
-$archivos = scandir("subidas");
-$num=0;
-for ($i=2; $i<count($archivos); $i++)
-{$num++;
-?>
-<!-- Visualización del nombre del archivo !-->
-         
-    <tr>
-      <th scope="row"><?php echo $num;?></th>
-      <td><?php echo $archivos[$i]; ?></td>
-      <td><a title="Descargar Archivo" href="subidas/<?php echo $archivos[$i]; ?>" download="<?php echo $archivos[$i]; ?>" style="color: blue; font-size:18px;"> <span class="glyphicon glyphicon-download-alt" aria-hidden="true"></span> </a></td>
-      <td><a title="Eliminar Archivo" href="Eliminar.php?name=subidas/<?php echo $archivos[$i]; ?>" style="color: red; font-size:18px;" onclick="return confirm('Esta seguro de eliminar el archivo?');"> <span class="glyphicon glyphicon-trash" aria-hidden="true"></span> </a></td>
-    </tr>
- <?php }?>
-</body>
-</html>
-  
 
-    </div>
 </header>
 
 </body>
