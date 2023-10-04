4a4477d0aa6acb77af38138d30717393add373e7
LGSjuanse <ajuansebastian89@gmail.com>
2023-10-04 17:01:33 -0500

    777

--- login.php
+++ login.php
@@ -1,27 +1,31 @@
 <?php
-session_start(); // Iniciando sesion
-include("conexion.php");//Contiene de conexion a la base de datos
+session_start();
+include("conexion.php");
 require_once("inicio_sesion.php");
-$error=''; // Variable para almacenar el mensaje de error
+$error='';
 if (isset($_POST['submit'])) {
 if (empty($_POST['Correo']) || empty($_POST['Contrasena'])) {
+	function validate($data)
+	$data = trim($data);
+	$data = stripslashes($data);
+	$data = htmlspecialchars($data);
+	return $data;
 $error = "Username or Password is invalid";
 }
 else
 {
-// Define $username y $password
 $username=$_POST['Correo'];
 $password=$_POST['Contrasena'];
-// Estableciendo la conexion a la base de datos
+
 
 $sql = "SELECT * FROM usuario WHERE Correo = '" . $username . "' and Contrasena = '".$password."';";
 $query=mysqli_query($con,$sql);
 $counter=mysqli_num_rows($query);
 if ($counter==1){
-		$_SESSION['login_user_sys']=$username; // Iniciando la sesion
-		header("location: index.html"); // Redireccionando a la pagina profile.php
+		$_SESSION['login_user_sys']=$username;
+		header("location: index.html");
 	
-	
+
 } else {
 $error = "El correo electrónico o la contraseña es inválida.";	
 }
