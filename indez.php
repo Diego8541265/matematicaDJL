fd4e424d4b8898e1d04c73f04b3d3289324aeceb
miyamiura <140010323+Shinrag@users.noreply.github.com>
2023-10-04 16:48:10 -0500

    4

--- indez.php
+++ indez.php
@@ -3,37 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>GUIAS</title>
-    <link rel="stylesheet" href="subir_Guas.css">
-
+    <title>Document</title>
 </head>
 <body>
-    <div  class="head1">
-
-        <div class="logo">
-            <a href="index.html">Matematicas DJL</a>
-        </div>
-
-        
-    <nav class="navbar">
-        <a href="index.html">Inicio</a>
-        <a href="inicio_sesion.html">sesión</a>
-   
-    </nav>
-</div>
-
-</header>
-
-
-    <div>
+    
+<div>
     <form enctype="multipart/form-data" action="" name="form" method="post">
         Seleccione Archivo
             <input type="file" name="file" id="file" /></td>
             <input type="submit" name="submit" id="submit" value="Subir" />
     </form>
 </div>
-
-</html>
-
+<?php include("loginuser.php") ?>
 </body>
 </html>
