c88ea9089cbb3ed9e9393c43b65c8c703f77bc9c
LGSjuanse <ajuansebastian89@gmail.com>
2023-10-11 14:25:29 -0500

    g

--- ini_ses.php
+++ ini_ses.php
@@ -20,7 +20,7 @@
 
   <nav class="navbar">
       <a href="index.html">Inicio</a>
-      <a href="inicio_sesion.php">Inicio de sesión</a>
+      <a href="ini_ses.php">Inicio de sesión</a>
   </nav>
 
 </div>
