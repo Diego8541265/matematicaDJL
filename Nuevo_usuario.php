e22fc8433bcb0dce8eb613877108906970eef0f5
matematicaDJL <142626548+Diego8541265@users.noreply.github.com>
2023-10-13 16:22:12 -0500

    olloooo

--- Nuevo_usuario.php
+++ Nuevo_usuario.php
@@ -11,7 +11,7 @@
      $hash = password_hash($pass, PASSWORD_DEFAULT, [10]);
  
      
-     if(!empty($nombres)&& !empty($apellidos) && !empty($correo)&& !empty($tipDoc)&& !empty($numDoc)&& !empty($contrasena) ){
+     if(!empty($nombres)&& !empty($apellidos) && !empty($correo)&& !empty($tipdoc)&& !empty($numdoc)&& !empty($contraseña) ){
          if(!filter_var($correo,FILTER_VALIDATE_EMAIL)){
           echo "<script> alert('Correo no valdo');</script>";
          }else{
