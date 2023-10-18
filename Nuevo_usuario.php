b673cba001c2d5a31dfb0c29554cf259f74db7de
Diego8541265 <142626548+Diego8541265@users.noreply.github.com>
2023-10-18 13:56:35 -0500

    thjhtghjht

--- Nuevo_usuario.php
+++ Nuevo_usuario.php
@@ -1,40 +1,4 @@
-<?php
-  include_once 'conexion.php';
 
-  if(isset($_POST['guardar'])){
-     $name = $_POST['nombres'];
-     $apell = $_POST['apellidos'];
-     $email = $_POST['correo'];
-     $tipDoc = $_POST['tipdoc'];
-     $numDoc = $_POST['numdoc'];
-     $pass = $_POST['contraseña'];
-     $hash = password_hash($pass, PASSWORD_DEFAULT, [10]);
- 
-     
-     if(!empty($nombres)&& !empty($apellidos) && !empty($correo)&& !empty($tipdoc)&& !empty($numdoc)&& !empty($contraseña) ){
-         if(!filter_var($correo,FILTER_VALIDATE_EMAIL)){
-          echo "<script> alert('Correo no valdo');</script>";
-         }else{
-             $consulta_insert=$con->prepare("INSERT INTO usuario(Nombres,Apellidos,Correo,id_tipdoc,numDoc,Contrasena)
-             VALUES(:nom,:apell,:email,:tipdoc,:numdoc,:pass)");
-             $consulta_insert->execute(array(
-               ':nom'=>$name,
-               ':apell'=>$apell,
-               ':email'=>$email,
-               ':tipdoc'=>$tipDoc,
-               ':numdoc'=>$numDoc,
-               ':pass'=>$hash,
-             ));
-             header('Location: Control_registro.php');
-         }    
-     }else{
-         echo "<script> alert('Los campos estan vacios');</script>";
-      }
-  }
-
-
-
-?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -54,7 +18,7 @@
 <!--tomado de : https://www.youtube.com/watch?v=Fc9X9xs4vgQ  -->
      <div class="contenedor2">
     <h2>Control de Registro de Usuarios</h2>
-     <form action="" method="post">
+     <form action="nuevo_usuario_controlador.php" method="post">
      <div class="form-group">
      <input type="text" name="nombres"  placeholder="Nombres" class="input_text">
      <input type="text" name="apellidos"  placeholder="Apellidos" class=input_text>
