abf02ece089e8cbc942eacc053b62429b9965d6e
LGSjuanse <ajuansebastian89@gmail.com>
2023-10-19 16:40:05 -0500

    qwegj

--- loginuser.php
+++ loginuser.php
@@ -20,7 +20,7 @@ if(!empty($_POST['nombres']) && !empty($_POST['apellidos']) && !empty($_POST['co
     $stms->bindParam(":numdoc",$numDoc);
     $stms->bindParam(":pass",$hash);
     if($stms->execute()){
-        header("location: inicio_sesion.html");
+        header("location: inicio_sesion.php");
     }else print ("Error en la consulta");
 }else{
     print("Debe completar todos los campos del formulario");
