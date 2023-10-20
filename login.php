54713a2dc81512c0ad388888163525f3bdae9005
LGSjuanse <ajuansebastian89@gmail.com>
2023-10-19 22:00:05 -0500

    gyygy

--- login.php
+++ login.php
@@ -17,17 +17,24 @@ if (isset($_POST['Correo']) && isset($_POST['Contrasena'])){
         $result = mysqli_query($conexion, $Sql);
 
         if (mysqli_num_rows($result) === 1) {
-            $row = mysqli_fetch_assoc($result);
-        if (password_verify($Contrasena, $row['Contrasena'])){
-
-            if ($row['Correo'] === $Correo && $row['Contrasena'] === $Contrasena) {
+        $row = mysqli_fetch_assoc($result);
+        if (password_verify($Contrasena, $row['Contrasena'])){//
+        if ($row['Correo'] === $Correo && $row['Contrasena'] === $Contrasena) {
                 $_SESSION['Correo'] = $row['Correo'];
                 header("Location: Apartado_Guias.html");
+                exit();
+            }//
             }else {
-                header("Location: inicio_sesion.php");
+                header("Location: inicio_sesion.php?error=El correo o la contraseña son incorrectos");
+                exit();
             }
-        }
+    }else{
+        header("Location: inicio_sesion.php?error=El correo o la contraseña son incorrectos");
+        exit();
     }
 }
+}else{
+    header("Location: inicio_sesion.php");
+    exit();
 }
 ?>
